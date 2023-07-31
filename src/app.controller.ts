844af4caf7a14c7e6b73aa7ab9810021cd058cbd
zahid-rahman <rahmanzahid94@gmail.com>
2023-07-31 23:43:12 +0600

    feat: add routes in app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
 
-@Controller()
+@Controller('users')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('')
+  getUser() {
+    return {
+      message: 'get all',
+      name: 'zahid',
+      company: 'programming-hero',
+      designation: 'software engineer',
+    };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  store(@Req() req: Request) {
+    return {
+      message: 'created',
+      request: req.body,
+    };
+  }
+
+  @Patch(':id')
+  patch(@Req() req: Request, @Param(':id') id: string) {
+    return {
+      message: 'updated',
+      request: req.body,
+      userId: id,
+    };
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return {
+      message: 'get by id',
+      userId: id,
+    };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return {
+      message: 'delete',
+      userId: id,
+    };
   }
 }
