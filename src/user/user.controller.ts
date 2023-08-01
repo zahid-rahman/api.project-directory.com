b01f875a0e012c374724bb8fa0da8512f7339584
zahid-rahman <rahmanzahid94@gmail.com>
2023-08-02 00:22:31 +0600

    feat: add validation

---
+++ src/user/user.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('')
+  getUser() {
+    return this.userService.getUser();
+  }
+
+  @Post()
+  store(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  patch(
+    @Body() updateUserDto: UpdateUserDto,
+    @Param('id') param: { id: number },
+  ) {
+    return this.userService.update(updateUserDto, param);
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  getUser() {
+    return {
+      message: 'get all',
+      name: 'zahid',
+      company: 'programming-hero',
+      designation: 'software engineer',
+    };
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  update(updateUserDto: UpdateUserDto, param: { id: number }) {
+    return {
+      body: updateUserDto,
+      param,
+    };
+  }
+
+  getUserById(id) {
+    return {
+      id,
+      name: 'zahid',
+      company: 'programming-hero',
+      designation: 'software engineer',
+    };
+  }
+
+  delete(id) {
+    return {
+      id,
+      message: 'delete user',
+    };
+  }
+}
